bd7a965882e5484ab1d7d6729626e91ac872ff01
rookie <675611058@qq.com>
2021-05-28 23:21:39 +0800

    主分支 ....

---
+++ src/main/java/com/hefeng/MyApplication.java
@@ -0,0 +1,12 @@
+package com.hefeng;
+
+/**
+ * @author rookie
+ * @date 2021/05/28 23:21
+ **/
+public class MyApplication {
+
+    public static void main(String[] args) {
+        System.out.println("这是主分支...");
+    }
+}
