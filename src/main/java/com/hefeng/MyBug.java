95e9bd933baa9c5ff9cb1facdb862d1af23e622f
rookie <675611058@qq.com>
2021-05-28 23:25:43 +0800

    hotfix 紧急解决

---
+++ src/main/java/com/hefeng/MyBug.java
@@ -0,0 +1,12 @@
+package com.hefeng;
+
+/**
+ * @author rookie
+ * @date 2021/05/28 23:24
+ **/
+public class MyBug {
+
+    public static void main(String[] args) {
+        System.out.println("紧急解决bug....");
+    }
+}
---
+++ src/main/java/com/hefeng/MyUtils.java
@@ -0,0 +1,11 @@
+package com.hefeng;
+
+/**
+ * @author rookie
+ * @date 2021/05/28 23:25
+ **/
+public class MyUtils {
+    public static void main(String[] args) {
+        System.out.println("解决bug需要用到...");
+    }
+}
